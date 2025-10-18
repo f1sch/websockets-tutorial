8a84e3a9c74e8a08d6b2b6ea523ce31729d56389
Anders <anders.fischer@posteo.de>
2025-10-18 14:38:39 +0200

    Implement event parser for local browser play

--- app.py
+++ app.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python
 import asyncio
+import itertools
 import json
 
 from websockets.asyncio.server import serve
-from connect4 import PLAYER1, PLAYER2
+from connect4 import PLAYER1, PLAYER2, Connect4
 
 # coroutine that manages a connection
 async def handler(websocket):
-    for player, column, row in [
-        (PLAYER1, 3, 0),
-        (PLAYER2, 3, 1),
-        (PLAYER1, 4, 0),
-        (PLAYER2, 4, 1),
-        (PLAYER1, 2, 0),
-        (PLAYER2, 1, 0),
-        (PLAYER1, 5, 0),
-    ]:
+    # Initialize a Connect Four game.
+    game = Connect4()
+
+    # Players take alternate turns, using the same browser
+    turns = itertools.cycle([PLAYER1, PLAYER2])
+    player = next(turns)
+
+    async for message in websocket:
+        # Parse a "play" event from the UI.
+        event = json.loads(message)
+        assert event["type"] == "play"
+        column = event["column"]
+
+        try:
+            # Play the move
+            row = game.play(player, column)
+        except ValueError as exc:
+            # Send an "error" event if the move was illegal
+            event = {
+                "type": "error",
+                "message": str(exc), 
+            }
+            await websocket.send(json.dumps(event))
+            continue
+
+        # Send a "play" event to update the UI
         event = {
             "type": "play",
             "player": player,
@@ -23,14 +41,18 @@ async def handler(websocket):
             "row": row,
         }
         await websocket.send(json.dumps(event))
-        await asyncio.sleep(0.5)
-    event = {
-        "type": "win",
-        "player": PLAYER1,
-    }
-    await websocket.send(json.dumps(event))
-    #async for message in websocket:
-    #    print(message)
+
+        # If move is winning, send a "win" event
+        if game.winner is not None:
+            event = {
+                "type": "win",
+                "player": game.winner,
+            }
+            await websocket.send(json.dumps(event))
+        
+        # Alternate turns.
+        player = next(turns)
+        
 
 # coroutine that calls serve() to start a websocket server
 async def main():
